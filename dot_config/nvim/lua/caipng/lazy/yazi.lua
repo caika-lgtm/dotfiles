3077dce079fea1294053ff9b1adb191d633b5e31
kaian <caika@sea.com>
2026-01-14 16:50:07 +0800

    yazi

---
+++ dot_config/nvim/lua/caipng/lazy/yazi.lua
@@ -0,0 +1,27 @@
+return {
+  "mikavilpas/yazi.nvim",
+  version = "*", -- use the latest stable version
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    {
+      "<leader>-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+  },
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = "?",
+    },
+  },
+}
